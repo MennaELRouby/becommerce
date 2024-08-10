a2fb284d7d976d36f75d258317478fe1d9891722
MennaELRouby <roubymenna@gmail.com>
2024-08-10 21:33:37 +0300

    direct url

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FrontController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', [FrontController::class, 'index'])->name('home');
+Route::get('shirts', [FrontController::class, 'shirts'])->name('shirts');
+Route::get('shirt', [FrontController::class, 'shirt'])->name('shirt');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
